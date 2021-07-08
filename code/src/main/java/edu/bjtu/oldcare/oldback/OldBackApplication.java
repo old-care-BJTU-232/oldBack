6100311ab33f399bf9cd7f75a418c6f5280f6a30
180 <1097546295@qq.com>
2021-07-08 11:47:36 +0800

    后端项目创建

---
+++ code/src/main/java/edu/bjtu/oldcare/oldback/OldBackApplication.java
@@ -0,0 +1,13 @@
+package edu.bjtu.oldcare.oldback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OldBackApplication.class, args);
+    }
+
+}
---
+++ code/src/test/java/edu/bjtu/oldcare/oldback/OldBackApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.bjtu.oldcare.oldback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OldBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
